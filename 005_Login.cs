f9d048a376a3aae5ed55b3832d03b47d90c055d6
rome5200 <144663246+rome5200@users.noreply.github.com>
2024-04-12 02:05:54 +0900

    Update 005_Login.cs

--- 005_Login.cs
+++ 005_Login.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace _005_Login
+namespace _005_Login // 교재 419p
 {
     public partial class Form1 : Form
     {
@@ -16,13 +16,16 @@ namespace _005_Login
         {
             InitializeComponent();
         }
-
+        // Click 이벤트 처리 함수
         private void btnBMI_Click(object sender, EventArgs e)
-        {
+        {    
+            // 아이디가 abcd이고 패스워드가 1234일 경우
+            // txtResult에 로그인 성공이라고 출력
             if (txtID.Text == "abcd" && txtPW.Text == "1234")
             {
                 txtResult.Text = "로그인 성공";
             }
+            // 아닐 경우 txtResult에 로그인 실패라고 출력
             else
             {
                 txtResult.Text = "로그인 실패";
