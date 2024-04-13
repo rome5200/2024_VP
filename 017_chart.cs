7c673316e77626f0b311958a9da0438c91697df3
rome5200 <144663246+rome5200@users.noreply.github.com>
2024-04-13 20:55:46 +0900

    Update 017_chart.cs

--- 017_chart.cs
+++ 017_chart.cs
@@ -17,15 +17,20 @@ namespace _018_chart
             InitializeComponent();
         }
 
+        // Form Load 이벤트 처리 함수
         private void Form1_Load(object sender, EventArgs e)
         {
-            chart1.Titles.Add("성적");
-            Random r = new Random();
+            chart1.Titles.Add("성적"); // chartdml 타이틀 : 성적
 
+            // Random 클래스의 객체 r 생성
+            Random r = new Random(); 
+
+            // 시리즈에 10개의 데이터를 넣어 차트로 출력 
             for (int i = 0; i < 10; i++)
             {
                 chart1.Series[0].Points.Add(r.Next(101));
             }
+            // 범례 : 비주얼\n 프로그래밍
             chart1.Series[0].LegendText = "비주얼\n프로그래밍";
         }
     }
