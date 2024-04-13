ca3c256a59de9c36e16a8a45d7ceb37419969a43
rome5200 <144663246+rome5200@users.noreply.github.com>
2024-04-13 20:07:31 +0900

    Update 012_ComboBox.cs

--- 012_ComboBox.cs
+++ 012_ComboBox.cs
@@ -13,19 +13,24 @@ namespace _012_ComboBox1
 {
     public partial class Form1 : Form
     {
-        TextBox[] titles;
-        ComboBox[] crds;
-        ComboBox[] grds;
+        
+        TextBox[] titles; // 교과목 TextBox 배열 titles 선언
+        ComboBox[] crds; // 학점 ComboBox 배열 crds 선언
+        ComboBox[] grds; // 성적 ComboBox 배열 grds 선언
 
         public Form1()
         {
             InitializeComponent();
         }
 
+        Button Click 이벤트 처리 함수
         private void button1_Click(object sender, EventArgs e)
         {
+            // totalScore와 totalCredits를 선언하고 초기화한다.
             double totalScore = 0;
             int totalCredits = 0;
+
+            // totalScore와 totalCredits을 계산
             for (int i = 0; i < crds.Length; i++)
             {
                 if (titles[i].Text != "")
@@ -35,9 +40,11 @@ namespace _012_ComboBox1
                     totalScore += crd * GetGrade(grds[i].SelectedItem.ToString());
                 }
             }
+            // txtGrade에 학점을 출력 
+            // 학점계산법 : 전체 점수 / 학점
             txtGrade.Text = ( totalScore / totalCredits ).ToString("0.00");
         }
-
+        // A+에서 F까지의 학점에 해당하는 점수를 리턴하는 메소드
         private double GetGrade(string v)
         {
             double grade = 0;
@@ -55,6 +62,7 @@ namespace _012_ComboBox1
             return grade;
         }
 
+        // Form Load 이벤트 처리 함수 
         private void Form1_Load(object sender, EventArgs e)
         {
             txt1.Text = "인체의 구조와 기능I";
@@ -66,12 +74,15 @@ namespace _012_ComboBox1
             txt7.Text = "영어I";
             txt8.Text = "의사소통장애의 이해";
 
-            crds = new ComboBox[] { crd1, crd2, crd3, crd4, crd5, crd6, crd7, crd8 };
-            grds = new ComboBox[] { grd1, grd2, grd3, grd4, grd5, grd6, grd7, grd8 };
-            titles = new TextBox[] { txt1, txt2, txt3, txt4, txt5, txt6, txt7, txt8 };
-            int[] arrCredit = { 1, 2, 3, 4, 5 };
-            List<string> lstGrade = new List<string> { "A+", "A0", "B+", "B0", "C+", "C0", "D+", "D0", "F" };
+            crds = new ComboBox[] { crd1, crd2, crd3, crd4, crd5, crd6, crd7, crd8 }; // 배열 crds 초기화
+            grds = new ComboBox[] { grd1, grd2, grd3, grd4, grd5, grd6, grd7, grd8 }; // 배열 grds 초기화
+            titles = new TextBox[] { txt1, txt2, txt3, txt4, txt5, txt6, txt7, txt8 }; // 배열 titles 초기화
+            int[] arrCredit = { 1, 2, 3, 4, 5 }; // 정수형 배열 arrCredit 선언
+            List<string> lstGrade = new List<string> { "A+", "A0", "B+", "B0", "C+", "C0", "D+", "D0", "F" }; // 리스트 lstGrade 생성
 
+            // 학점 ComboBox 배열 crds의 각 요소에 대해
+            // arrCredit의 각 요소를 Items로 등록하고
+            // 정수 3의 값을 디폴트로 지정
             foreach (var combo in crds)
             {
                 foreach (var i in arrCredit)
@@ -79,6 +90,8 @@ namespace _012_ComboBox1
                 combo.SelectedItem = 3;
             }
 
+            // 성적 ComboBox에 배열 grds의 각 요소에 대해
+            // lstGrade 리스트의 각 요소를 Items로 등록
             foreach (var cb in grds)
             {
                 foreach (var gr in lstGrade)
