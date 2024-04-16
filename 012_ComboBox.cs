e99afd70960ff8b5950c213c1dc7f75476fcdcdf
rome5200 <144663246+rome5200@users.noreply.github.com>
2024-04-17 00:00:05 +0900

    Update 012_ComboBox.cs

--- 012_ComboBox.cs
+++ 012_ComboBox.cs
@@ -23,6 +23,43 @@ namespace _012_ComboBox1
             InitializeComponent();
         }
 
+        // Form Load 이벤트 처리 함수 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txt1.Text = "인체의 구조와 기능I";
+            txt2.Text = "일반수학I";
+            txt3.Text = "전기전자공학 및 실험";
+            txt4.Text = "데이터사이언스";
+            txt5.Text = "비주얼프로그래밍";
+            txt6.Text = "설계 및 프로젝트 기본I";
+            txt7.Text = "영어I";
+            txt8.Text = "의사소통장애의 이해";
+
+            crds = new ComboBox[] { crd1, crd2, crd3, crd4, crd5, crd6, crd7, crd8 }; // 배열 crds 초기화
+            grds = new ComboBox[] { grd1, grd2, grd3, grd4, grd5, grd6, grd7, grd8 }; // 배열 grds 초기화
+            titles = new TextBox[] { txt1, txt2, txt3, txt4, txt5, txt6, txt7, txt8 }; // 배열 titles 초기화
+            int[] arrCredit = { 1, 2, 3, 4, 5 }; // 정수형 배열 arrCredit 선언
+            List<string> lstGrade = new List<string> { "A+", "A0", "B+", "B0", "C+", "C0", "D+", "D0", "F" }; // 리스트 lstGrade 생성
+
+            // 학점 ComboBox 배열 crds의 각 요소에 대해
+            // arrCredit의 각 요소를 Items로 등록하고
+            // 정수 3의 값을 디폴트로 지정
+            foreach (var combo in crds)
+            {
+                foreach (var i in arrCredit)
+                    combo.Items.Add(i);
+                combo.SelectedItem = 3;
+            }
+
+            // 성적 ComboBox에 배열 grds의 각 요소에 대해
+            // lstGrade 리스트의 각 요소를 Items로 등록
+            foreach (var cb in grds)
+            {
+                foreach (var gr in lstGrade)
+                    cb.Items.Add(gr);
+            }
+        }
+
         // Button Click 이벤트 처리 함수
         private void button1_Click(object sender, EventArgs e)
         {
@@ -61,42 +98,6 @@ namespace _012_ComboBox1
 
             return grade;
         }
-
-        // Form Load 이벤트 처리 함수 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            txt1.Text = "인체의 구조와 기능I";
-            txt2.Text = "일반수학I";
-            txt3.Text = "전기전자공학 및 실험";
-            txt4.Text = "데이터사이언스";
-            txt5.Text = "비주얼프로그래밍";
-            txt6.Text = "설계 및 프로젝트 기본I";
-            txt7.Text = "영어I";
-            txt8.Text = "의사소통장애의 이해";
-
-            crds = new ComboBox[] { crd1, crd2, crd3, crd4, crd5, crd6, crd7, crd8 }; // 배열 crds 초기화
-            grds = new ComboBox[] { grd1, grd2, grd3, grd4, grd5, grd6, grd7, grd8 }; // 배열 grds 초기화
-            titles = new TextBox[] { txt1, txt2, txt3, txt4, txt5, txt6, txt7, txt8 }; // 배열 titles 초기화
-            int[] arrCredit = { 1, 2, 3, 4, 5 }; // 정수형 배열 arrCredit 선언
-            List<string> lstGrade = new List<string> { "A+", "A0", "B+", "B0", "C+", "C0", "D+", "D0", "F" }; // 리스트 lstGrade 생성
-
-            // 학점 ComboBox 배열 crds의 각 요소에 대해
-            // arrCredit의 각 요소를 Items로 등록하고
-            // 정수 3의 값을 디폴트로 지정
-            foreach (var combo in crds)
-            {
-                foreach (var i in arrCredit)
-                    combo.Items.Add(i);
-                combo.SelectedItem = 3;
-            }
-
-            // 성적 ComboBox에 배열 grds의 각 요소에 대해
-            // lstGrade 리스트의 각 요소를 Items로 등록
-            foreach (var cb in grds)
-            {
-                foreach (var gr in lstGrade)
-                    cb.Items.Add(gr);
-            }
-        }
+        
     }
 }
