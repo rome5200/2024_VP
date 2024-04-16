4a0815176f89c5506da13e011047750c7019db47
rome5200 <144663246+rome5200@users.noreply.github.com>
2024-04-16 19:22:46 +0900

    Update 016_test.cs

--- 016_test.cs
+++ 016_test.cs
@@ -16,5 +16,40 @@ namespace _016_test
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for(int i = 1; i <= 4; i++)
+            {
+                cb_grade.Items.Add(i);
+            }
+            cb_grade.SelectedItem = 2;
+
+            cb_life.Items.Add("기숙사");
+            cb_life.Items.Add("자취");
+            cb_life.Items.Add("통학");
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RadioButton[] major = new RadioButton[] { rb1, rb2, rb3, rb4, rb5};
+            String result = "";
+            if(rb1.Checked)
+                result = rb1.Text\n;
+            else if(rb2.Checked)
+                result = rb2.Text\n;
+            else if(rb3.Checked)
+                result = rb3.Text\n;
+            else if(rb4.Checked)
+                result = rb4.Text\n;
+            else if(rb5.Checked)
+                result = rb5.Text\n;
+            
+            string result += cb_grade.Text + "학년\n"+
+                txt_num.Text + "\n" + txt_name.Text.ToString() + "\n" + cb_life.Text.ToString();
+
+           MessageBox.Show(result, "학생정보");
+        
+        }
     }
 }
