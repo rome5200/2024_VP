42a16e9428a90f7eb9b1b2e38b21866f6a2c7e1b
rome5200 <144663246+rome5200@users.noreply.github.com>
2024-04-17 02:08:27 +0900

    Update 018_twoChartAreas.cs

--- 018_twoChartAreas.cs
+++ 018_twoChartAreas.cs
@@ -22,7 +22,7 @@ namespace _018_twoChartAreas
         {
             chart1.Titles.Add("성적");
             
-            "Series2"라는 이름의 series 추가
+            // "Series2"라는 이름의 series 추가
             chart1.Series.Add("Series2");
 
             chart1.Series[0].LegendText = "수학";
@@ -42,16 +42,16 @@ namespace _018_twoChartAreas
         {
             if (chart1.ChartAreas.Count <= 1)
                 chart1.ChartAreas.Add("ChartArea2"); // ChartArea1은 디폴트로 존재하기 때문에 ChartArea2만 생성
-            chart1.Series[1].ChartArea = "ChartArea2";
+            chart1.Series[1].ChartArea = "ChartArea2"; // 영어 점수를 두 번째 ChartArea에 그림
         }
 
         // 합쳐서 그리기
         // Button Click 이벤트 처리 함수
         private void btnOneChart_Click(object sender, EventArgs e)
         {
-            if (chart1.ChartAreas.Count >=2 )
-                chart1.ChartAreas.RemoveAt(1);
-            chart1.Series[1].ChartArea = "ChartArea1";
+            if (chart1.ChartAreas.Count >=2 ) 
+                chart1.ChartAreas.RemoveAt(1); // 두 번째 ChartArea 삭제
+            chart1.Series[1].ChartArea = "ChartArea1"; // 영어 점수를 첫 번째 ChartArea에 그림
         }
     }
 }
